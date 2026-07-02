bd271a92fbd61ad79358cde4de7a0400d2bd0c35
rozi123A <likeask59@gmail.com>
2026-07-02 22:38:01 +0100

    إضافة زر فلاتر داخل الدردشة + نقاط حالة اتصال خضراء/حمراء

--- client/src/components/FriendsPanel.tsx
+++ client/src/components/FriendsPanel.tsx
@@ -192,7 +192,7 @@ export default function FriendsPanel({
                             alt={friend.name}
                             className="w-10 h-10 rounded-full border-2 border-white/20 object-cover"
                           />
-                          <span className="absolute bottom-0 right-0 w-3 h-3 bg-gray-500 rounded-full border-2 border-gray-900" />
+                          <span className="absolute bottom-0 right-0 w-3 h-3 bg-red-500 rounded-full border-2 border-gray-900" />
                         </div>
                         <div className="flex-1 min-w-0">
                           <p className="text-white font-semibold text-sm truncate">{friend.name}</p>
